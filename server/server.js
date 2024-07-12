c58f286f01fce705ebd2a16f50a3f26c92455761
Gotfocuss <124418797+Gotfocuss@users.noreply.github.com>
2024-07-12 08:20:34 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
+
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 
 import { appConfig } from "./config/appConfig.js";
+import { aiConfig } from "./config/aiConfig.js";
 import { aiController } from "./controllers/aiController.js";
 
 const app = express();
@@ -16,11 +18,13 @@ app.use(
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT;
+const ASKk = process.env.GEMINI_API_KEY;
 
 // Get Gemini API Response
 app.post("/chat-with-gemini", aiController);
 
 // App listening
 app.listen(PORT, () => {
+  //console.log(PORT)
   console.log("Gemini AI Server is listening on port number", PORT);
 });
